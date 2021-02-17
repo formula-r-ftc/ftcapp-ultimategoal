273a0cf0b829eba239acb66982fbdec79fc8c728
Pro-grammar26 <sahith.chandra@gmail.com>
2021-02-16 17:34:57 -0800

    targets

--- FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/targetZoneb.java
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/targetZoneb.java
@@ -1,4 +1,4 @@
-  package org.firstinspires.ftc.teamcode.autonomous;
+package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -28,6 +27,7 @@ public class targetZoneb extends OpMode {
     double LFPreviousValue = 0;
     double LBPreviousValue = 0;
 
+
     double one = 537.6;
 
     ElapsedTime t1 = new ElapsedTime();
@@ -35,7 +35,7 @@ public class targetZoneb extends OpMode {
 
     double AverageEconderPosition;
 
-    //  this gives you the distance and speed of encoders
+    //  this gives you the distance and speed of motors
     double encoderSpeed(double targetPosition, double maxSpeed) {
         AverageEconderPosition = (RFMotor.getCurrentPosition() - RFPreviousValue + LFMotor.getCurrentPosition() - LFPreviousValue + RBMotor.getCurrentPosition() - RBPreviousValue + LBMotor.getCurrentPosition() - LBPreviousValue) / 4;
         double distance = targetPosition - AverageEconderPosition;
@@ -67,14 +67,13 @@ public class targetZoneb extends OpMode {
     }
 
     public void rampUp(double distance, double heading, double time, double maxSpeed, double busyTime) {
-        double AvgEncPos = (RFMotor.getCurrentPosition() + LFMotor.getCurrentPosition() + RBMotor.getCurrentPosition() + LBMotor.getCurrentPosition()) / 4;
+        double AvgEncPos = (RFMotor.getCurrentPosition() - RFPreviousValue + LFMotor.getCurrentPosition() - LFPreviousValue + RBMotor.getCurrentPosition() - RBPreviousValue + LBMotor.getCurrentPosition() - LBPreviousValue) / 4;
         double AccelerationSlope = maxSpeed / time;
         double power = t1.seconds() * AccelerationSlope;
         if (Math.abs(power) < Math.abs(encoderSpeed(distance, maxSpeed))) { // if acceleration is less than speed
             setTurnPower(turn(heading), power);  //then set motor power to turn towards heading and accelerate until max speed
         } else {
             if (!(Math.abs(distance - AvgEncPos) < 80)) {
-//                    if (runtime.seconds() < busyTime) {
                 telemetry.addData("motor is: ", "busy");
                 setTurnPower(turn(heading), encoderSpeed(distance, maxSpeed));// otherwise keep motor power to heading and stop at the target Encoder Position
             } else {
@@ -84,11 +83,6 @@ public class targetZoneb extends OpMode {
                 RBMotor.setPower(0);
                 LBMotor.setPower(0);
                 setTurnPower(0, 0);
-
-                RFPreviousValue = RFMotor.getCurrentPosition();
-                RBPreviousValue = RBMotor.getCurrentPosition();
-                LFPreviousValue = RFMotor.getCurrentPosition();
-                LBPreviousValue = RFMotor.getCurrentPosition();
 
             }
         }
@@ -101,8 +95,7 @@ public class targetZoneb extends OpMode {
         if (Math.abs(power) < Math.abs(encoderSpeed(distance, maxSpeed))) { // if acceleration is less than speed
             setTurnPower(turn(heading), power);  //then set motor power to turn towards heading and accelerate until max speed
         } else {
-            if (!(Math.abs(heading - getHeading()) < 11)) {
-                //                   if (runtime.seconds() < busyTime) {
+            if (!(Math.abs(heading - getHeading()) < 5)) {
                 telemetry.addData("motor is: ", "busy");
                 setTurnPower(turn(heading), encoderSpeed(distance, maxSpeed));// otherwise keep motor power to heading and stop at the target Encoder Position
             } else {
@@ -119,6 +112,7 @@ public class targetZoneb extends OpMode {
 
     boolean tripLoopDone = false;
     boolean EncoderPower;
+
     boolean tripLoop() {
         double AverageEncPower = (RFMotor.getPower() + LFMotor.getPower() + RBMotor.getPower() + LBMotor.getPower()) / 4;
 
@@ -133,13 +127,16 @@ public class targetZoneb extends OpMode {
         }
 
         if (tripLoopDone && !EncoderPower) {
-            return true;
-        } else {
+
             RFPreviousValue = RFMotor.getCurrentPosition();
             RBPreviousValue = RBMotor.getCurrentPosition();
             LFPreviousValue = RFMotor.getCurrentPosition();
             LBPreviousValue = RFMotor.getCurrentPosition();
+            return true;
+        } else {
+            telemetry.addData("tripLoop return:", "FALSE");
             return false;
+
         }
     }
 
@@ -211,36 +208,28 @@ public class targetZoneb extends OpMode {
     public void loop() {
 //rampUpTurn(0,95,0.5,0.3,5);
         if (!trip1) {
-            rampUp(5.17 * one, 0, 0.5, 0.5, 5);
+            rampUp(2.9 * one, 15, 0.5, 0.5, 5);
             trip1 = tripLoop();
             telemetry.addData("trip", "1");
+        } else if (trip1 && !trip2){
+            rampUpTurn(0, -10, 0.5, 0.4, 5);
+        trip2 = tripLoop();
+        telemetry.addData("trip", "2");
+        } else if (trip2 && !trip3){
+            rampUp(3.8 * one, -15, 0.5, 0.6, 5);
+            trip3 = tripLoop();
+            telemetry.addData("trip", "3");
         }
-        else if (trip1 && !trip2) {
-            rampUp(-1.75 * one, 0, 0.5, 0.2, 5);
-            trip2 = tripLoop();
-            telemetry.addData("trip", "2");
 
-        }
-//        } else if (trip2 && !trip3){
-//            rampUp(one, 90, 0.5, 0.3, 5);
-//            trip3 = tripLoop();
-//            telemetry.addData("trip", "3");
-//        } else if(trip3 && !trip4){
-//            rampUpTurn(-one, 0,0.5,0.3,5);
-//            trip4 = tripLoop();
-//            telemetry.addData("trip", "4");
+        telemetry.addData("Runtime: ", runtime.seconds());
+        telemetry.addData("RFMotor encoder:", RFMotor.getCurrentPosition());
+        telemetry.addData("LFMotor encoder:", LFMotor.getCurrentPosition());
+        telemetry.addData("RBMotor encoder:", RBMotor.getCurrentPosition());
+        telemetry.addData("LBMotor encoder:", LBMotor.getCurrentPosition());
+        telemetry.addData("heading:", getHeading());
+        telemetry.addData("avg encoder:", (LBMotor.getCurrentPosition() + RBMotor.getCurrentPosition() + LFMotor.getCurrentPosition() + RFMotor.getCurrentPosition()) / 4);
+        telemetry.update();
 
-            telemetry.addData("Runtime: ", runtime.seconds());
-            telemetry.addData("RFMotor encoder:", RFMotor.getCurrentPosition());
-            telemetry.addData("LFMotor encoder:", LFMotor.getCurrentPosition());
-            telemetry.addData("RBMotor encoder:", RBMotor.getCurrentPosition());
-            telemetry.addData("LBMotor encoder:", LBMotor.getCurrentPosition());
-            telemetry.addData("heading:", getHeading());
-            telemetry.addData("avg encoder:", (LBMotor.getCurrentPosition() + RBMotor.getCurrentPosition() + LFMotor.getCurrentPosition() + RFMotor.getCurrentPosition()) / 4);
-            telemetry.update();
-        }
 
     }
-
-
-
+}
